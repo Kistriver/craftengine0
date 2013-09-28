884fff2dbaedae781ce469eeaeaf4f5207e3cab9
Kachalov Alexey <kachalov@kistriver.com>
2013-09-28 23:30:00 +0400

    No description

--- client/system/plugins/download/pages/download.php
+++ client/system/plugins/download/pages/download.php
@@ -7,7 +7,7 @@ $editions[] = array('name'=>'Standart Edition','info'=>'---','desc'=>'Ничег
 
 $editions[] = array('name'=>'Minecraft Edition','info'=>'API: 0.1.5_alpha<br /> Client: v1.4 <br />Size: ~2MB',
 					'desc'=>'Плагины: users, articles. Модифицированный под плагины клиент. Интергация плагина users с Bukkit плагином AuthMe. ',
-					'status'=>'success','time'=>'27.09.13 22.45','update'=>'Update 5','link'=>'/me/latest.zip');
+					'status'=>'success','time'=>'28.09.13 22.08','update'=>'Update 6','link'=>'/me/latest.zip');
 
 $core->render['editions'] = $editions;
 $core->f->show('download/main','download');
